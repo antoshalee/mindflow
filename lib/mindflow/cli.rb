583009ab176fdd836f4b9be2e70d0abc1b27a559
Anton Lee <antoshalee@gmail.com>
2016-08-28 10:22:58 +0300

    Basic CLI

---
+++ lib/mindflow/cli.rb
@@ -0,0 +1,24 @@
+require 'mindflow'
+require 'pathname'
+
+class Mindflow::CLI
+  def initialize(argv)
+    @argv = argv
+  end
+
+  def run
+    puts Unparser.unparse(ast)
+  end
+
+  def ast
+    Mindflow::Parser.new.parse(input)
+  end
+
+  def input
+    Pathname.new(path).read
+  end
+
+  def path
+    "#{Dir.pwd}/#{@argv[0]}"
+  end
+end
