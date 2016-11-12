a90404d04a9f979f690b5f6ee160019a206b7dd7
Anton Lee <antoshalee@gmail.com>
2016-11-12 23:26:13 +0300

    Delete all obsolete specs

--- spec/mindflow_spec.rb
+++ spec/mindflow_spec.rb
@@ -4,12 +4,4 @@ describe Mindflow do
   it 'has a version number' do
     expect(Mindflow::VERSION).not_to be nil
   end
-
-  # Future feature
-  it 'parses' do
-    pending
-    path = Pathname.new "#{__dir__}/fixtures/some_ruby_code.rb"
-    ::Parser::CurrentRuby.parse path.read
-    expect(true).to eq true
-  end
 end
